c9f09eaa8b2f22e0c156c038e51439383ad4ec39
clerie <git@clerie.de>
2019-03-01 11:44:41 +0100

    Variable request refactor

--- demostatapp/views.py
+++ demostatapp/views.py
@@ -21,21 +21,21 @@ class IndexView(generic.ListView):
 def demos(response):
     demo_list = get_list_or_404(Demo)
 
-    return render(response, 'demostatapp/demos_list.html', {'demo_list': demo_list})
+    return render(request, 'demostatapp/demos_list.html', {'demo_list': demo_list})
 
-def demos_year(response, date__year):
+def demos_year(request, date__year):
     demo_list = get_list_or_404(Demo, date__year=date__year)
     demo_prev = Demo.objects.filter(date__year__lt=date__year).order_by('date').last()
     demo_next = Demo.objects.filter(date__year__gt=date__year).order_by('date').first()
 
-    return render(response, 'demostatapp/demos_year_list.html', {'date': datetime.date(int(date__year), 1, 1), 'demo_list': demo_list, 'demo_prev': demo_prev, 'demo_next': demo_next})
+    return render(request, 'demostatapp/demos_year_list.html', {'date': datetime.date(int(date__year), 1, 1), 'demo_list': demo_list, 'demo_prev': demo_prev, 'demo_next': demo_next})
 
-def demos_month(response, date__year, date__month):
+def demos_month(request, date__year, date__month):
     demo_list = get_list_or_404(Demo, date__year=date__year, date__month=date__month)
     demo_prev = Demo.objects.filter(date__year__lte=date__year, date__month__lt=date__month).order_by('date').last()
     demo_next = Demo.objects.filter(date__year__gte=date__year, date__month__gt=date__month).order_by('date').first()
 
-    return render(response, 'demostatapp/demos_month_list.html', {'date': datetime.date(int(date__year), int(date__month), 1), 'demo_list': demo_list, 'demo_prev': demo_prev, 'demo_next': demo_next})
+    return render(request, 'demostatapp/demos_month_list.html', {'date': datetime.date(int(date__year), int(date__month), 1), 'demo_list': demo_list, 'demo_prev': demo_prev, 'demo_next': demo_next})
 
 def demos_day(request, date__year, date__month, date__day):
     return HttpResponseRedirect(reverse('demostatapp:demos_month', args=(date__year, date__month)) + '#' + date__day)
@@ -54,7 +54,7 @@ class OrganisationView(generic.DetailView):
     model = Organisation
     template_name = 'demostatapp/organisation_detail.html'
 
-def tag(response, tag_slug):
+def tag(request, tag_slug):
     demo_list = get_list_or_404(Demo, tags__slug__exact=tag_slug, date__gt=timezone.now().date(), date__lt=timezone.now().date()+datetime.timedelta(weeks=4))
     tag_name = tag_slug
 
@@ -63,4 +63,4 @@ def tag(response, tag_slug):
             tag_name = tag.name
             break
 
-    return render(response, 'demostatapp/tag_detail.html', {'tag_slug': tag_slug, 'tag_name': tag_name, 'demo_list': demo_list})
+    return render(request, 'demostatapp/tag_detail.html', {'tag_slug': tag_slug, 'tag_name': tag_name, 'demo_list': demo_list})
