0a710e99406b8d4b9132e49a1eb51529f579ab8e
clerie <git@clerie.de>
2019-03-14 09:26:39 +0100

    Seitenvariablen ergänzt

--- demostat/views.py
+++ demostat/views.py
@@ -4,9 +4,26 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.utils.http import urlencode
+from django.conf import settings
 import datetime
 
 from .models import Organisation, Demo
+
+def make_context_object(context):
+    s = {}
+    try:
+        s['SITE_TITLE'] = settings.SITE_TITLE
+    except:
+        pass
+    try:
+        s['SITE_IMPRINT_URL'] = settings.SITE_IMPRINT_URL
+    except:
+        pass
+    try:
+        s['SITE_PRIVACY_URL'] = settings.SITE_PRIVACY_URL
+    except:
+        pass
+    return {**s, **context}
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -25,21 +42,21 @@ class IndexView(generic.ListView):
 def demos(request):
     demo_list = get_list_or_404(Demo)
 
-    return render(request, 'demostat/demos_list.html', {
+    return render(request, 'demostat/demos_list.html', make_context_object({
         'demo_list': demo_list
-    })
+    }))
 
 def demos_year(request, date__year):
     demo_list = get_list_or_404(Demo, date__year=date__year)
     demo_prev = Demo.objects.filter(date__year__lt=date__year).order_by('date').last()
     demo_next = Demo.objects.filter(date__year__gt=date__year).order_by('date').first()
 
-    return render(request, 'demostat/demos_year_list.html', {
+    return render(request, 'demostat/demos_year_list.html', make_context_object({
         'date': datetime.date(int(date__year), 1, 1),
         'demo_list': demo_list,
         'demo_prev': demo_prev,
         'demo_next': demo_next,
-    })
+    }))
 
 def demos_month(request, date__year, date__month):
     demo_list = Demo.objects.filter(date__year=date__year, date__month=date__month).order_by('date')
@@ -56,14 +73,14 @@ def demos_month(request, date__year, date__month):
     demo_prev = Demo.objects.filter(date__year__lte=date__year, date__month__lt=date__month).order_by('date').last()
     demo_next = Demo.objects.filter(date__year__gte=date__year, date__month__gt=date__month).order_by('date').first()
 
-    return render(request, 'demostat/demos_month_list.html', {
+    return render(request, 'demostat/demos_month_list.html', make_context_object({
         'date': datetime.date(int(date__year), int(date__month), 1),
         'demo_list': demo_list,
         'demo_prev': demo_prev,
         'demo_next': demo_next,
         'filter_tag': sorted(request.GET.getlist('tag')),
         'filter_org': request.GET.get('org'),
-    })
+    }))
 
 def demos_day(request, date__year, date__month, date__day):
     return HttpResponseRedirect(reverse('demostat:demos_month', args=(date__year, date__month)) + '#' + date__day)
@@ -72,9 +89,9 @@ def demos_day(request, date__year, date__month, date__day):
 def demo(request, date__year, date__month, date__day, slug):
     demo = get_object_or_404(Demo, date__year=date__year, date__month=date__month, date__day=date__day, slug=slug)
 
-    return render(request, 'demostat/demo_detail.html', {
-        'demo': demo
-    })
+    return render(request, 'demostat/demo_detail.html', make_context_object({
+        'demo': demo,
+    }))
 
 def demo_id(request, demo_id):
     demo = get_object_or_404(Demo, pk=demo_id)
@@ -93,8 +110,8 @@ def tag(request, tag_slug):
             tag_name = tag.name
             break
 
-    return render(request, 'demostat/tag_detail.html', {
+    return render(request, 'demostat/tag_detail.html', make_context_object({
         'tag_slug': tag_slug,
         'tag_name': tag_name,
         'demo_list': demo_list
-    })
+    }))
