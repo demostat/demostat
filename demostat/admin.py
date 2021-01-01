c2de9a09099a9c3e96b246d37ae733987aac713b
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-01-01 16:25:42 +0100

    Auto populate slug fields in admin ui
    When no slug is entered during the creation of an object (e.g. Demo), it
    will be abbreviated fromt the name or title according to djangos
    guidelines live in JavaScript[^0]. A manually entered or present slug
    will not be changed automatically.
    
    [^0]: https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.prepopulated_fields

--- demostat/admin.py
+++ demostat/admin.py
@@ -3,9 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Organisation, Region, Location, Tag, Demo, Link
 
-admin.site.register(Organisation)
-admin.site.register(Region)
+@admin.register(Demo)
+class DemoAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+@admin.register(Organisation)
+class OrganisationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Location)
-admin.site.register(Tag)
-admin.site.register(Demo)
 admin.site.register(Link)
