28c1debfc2e7e69d44f9e3bb2d6f28d4127d4459
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-01-01 17:17:08 +0100

    Add filtering and serach to admin site
    It can be grouped by date and, when applicable, filters are available
    (e.G. locations for Demos, Region for Locations, ...). Search by free
    text is also available.
    
    Especially with Demos this may scale not so well, but that's to be
    determined in future I think.

--- demostat/admin.py
+++ demostat/admin.py
@@ -5,19 +5,100 @@ from .models import Organisation, Region, Location, Tag, Demo, Link
 
 @admin.register(Demo)
 class DemoAdmin(admin.ModelAdmin):
+    search_fields = (
+            'slug',
+            'title',
+            'location__name',
+            'organisation__name',
+            'description',
+            'note',
+            'tags',
+        )
+
     prepopulated_fields = {"slug": ("title",)}
+
+    date_hierarchy = 'date'
+
+    list_display = (
+            'title',
+            'date',
+            'location'
+        )
+
+    list_filter = (
+            ('location', admin.RelatedOnlyFieldListFilter),
+            ('tags', admin.RelatedOnlyFieldListFilter),
+            ('organisation', admin.RelatedOnlyFieldListFilter),
+        )
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = (
+            'title',
+            'demo_name',
+            'demo_date',
+        )
+
+    search_fields = (
+            'title',
+            'url',
+            'demo__title'
+        )
+
+    date_hierarchy = 'demo__date'
+
+    def demo_name(self, obj):
+        return obj.demo.title
+
+    def demo_date(self, obj):
+        return obj.demo.date
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+            'name',
+            'region'
+        )
+
+    list_filter = (
+            ('region', admin.RelatedOnlyFieldListFilter),
+        )
+
+    search_fields = (
+            'name',
+            'region__name',
+        )
 
 @admin.register(Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
+    list_display = (
+            'name',
+        )
+
     prepopulated_fields = {"slug": ("name",)}
 
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
+    list_display = (
+            'name',
+            'group',
+        )
+
+    list_filter = (
+            ('group', admin.RelatedOnlyFieldListFilter),
+        )
+
     prepopulated_fields = {'slug': ('name',)}
+
+    search_fields = (
+            'name',
+            'group',
+        )
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Location)
-admin.site.register(Link)
+    search_fields = (
+            'name',
+        )
